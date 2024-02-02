25cce75fbe9021b4cd52404643f03c421b030356
BrianKani <kipsangkangogo26@gmail.com>
2024-02-02 05:26:46 +0300

    Geosgraphic coordiantes for the study area

---
+++ Data/Note.cs
@@ -0,0 +1,6 @@
+// been trying to install gdal on my environment for the last 24 hours
+// To dos: Download satellite images
+	// Get yield data
+	// Get weather data for the and carry out spatial alignment with the satellite images
+
+// 25th Jan 2023 Finish up on the write up (methodology part)
